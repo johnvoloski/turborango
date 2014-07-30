b0b6c24579106a702fcdfa00dfc389650909e647
Bernardo Bosak de Rezende <bernardobrezende@gmail.com>
2014-07-30 00:20:55 -0300

    (fix): specs.

--- spec/requests/menus_spec.rb
+++ spec/requests/menus_spec.rb
@@ -5,23 +5,9 @@ describe "Menus" do
   let(:restaurant) { create(:restaurant) }
   let(:menus)      { create_list(:menu, 3, restaurant_id: restaurant.id) }
 
-  context 'GET /restaurants/:restaurant_id/menus' do
-    it 'returns 200' do
-      get restaurant_menus_path(restaurant)
-      expect(response.status).to eq(200)
-    end
-  end
-
   context 'GET /restaurants/:restaurant_id/menus.json' do
     it 'returns 200' do
       get restaurant_menus_path(restaurant, format: :json)
-      expect(response.status).to eq(200)
-    end
-  end
-
-  context 'GET /restaurants/:restaurant_id/menus/:id' do
-    it 'returns 200' do
-      get restaurant_menu_path(restaurant, menus.first)
       expect(response.status).to eq(200)
     end
   end
@@ -33,27 +19,6 @@ describe "Menus" do
     end
   end
 
-  context 'GET /restaurants/:restaurant_id/menus/new' do
-    it 'returns 200' do
-      get new_restaurant_menu_path(restaurant)
-      expect(response.status).to eq(200)
-    end
-  end
-
-  context 'GET /restaurants/:restaurant_id/menus/:id/edit' do
-    it 'returns 200' do
-      get edit_restaurant_menu_path(restaurant, menus.first)
-      expect(response.status).to eq(200)
-    end
-  end
-
-  context 'POST /restaurants/:restaurant_id/menus' do
-    it 'returns 302' do
-      post restaurant_menus_path(restaurant), { menu: attributes_for(:menu, restaurant_id: restaurant.id) }
-      expect(response.status).to eq(302)
-    end
-  end
-
   context 'POST /restaurants/:restaurant_id/menus.json' do
     it 'returns 201' do
       post restaurant_menus_path(restaurant, format: :json), { menu: attributes_for(:menu, restaurant_id: restaurant.id) }
@@ -61,24 +26,10 @@ describe "Menus" do
     end
   end
 
-  context 'PATH/PUT /restaurants/:restaurant_id/menus/:id' do
-    it 'returns 302' do
-      put restaurant_menu_path(restaurant, menus.first), { menu: attributes_for(:menu, restaurant_id: restaurant.id) }
-      expect(response.status).to eq(302)
-    end
-  end
-
   context 'PATH/PUT /restaurants/:restaurant_id/menus/:id.json' do
     it 'returns 200' do
       put restaurant_menu_path(restaurant, menus.first, format: :json), { menu: attributes_for(:menu, restaurant_id: restaurant.id) }
       expect(response.status).to eq(200)
-    end
-  end
-
-  context 'DELETE /restaurants/:restaurant_id/menus/:id' do
-    it 'returns 302' do
-      delete restaurant_menu_path(restaurant, menus.first)
-      expect(response.status).to eq(302)
     end
   end
 
